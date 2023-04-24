fc64fc43d9062f12115e17a9172b99ddd8828b82
xrr2016 <xiaoranran1993@outlook.com>
2023-04-24 18:44:48 +0800

    feat: update

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import "./style.css";
 import viteLogo from "./images/vite.svg";
 import javascriptLogo from "./images/javascript.svg";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-  </div>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//   </div>
+// `;
